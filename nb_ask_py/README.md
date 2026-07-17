4020835da99ff8eea2ce5acae5a2abe03e116062
benkarr <64742466+Ben-Karr@users.noreply.github.com>
2026-07-17 19:04:59 +0200

    prettify llm output

---
+++ nb_ask_py/README.md
@@ -0,0 +1,2 @@
+## ToDo:
+* Standalone Route for /get_active_cell -> get active cell first, then make llm request, insert result behind the request cell (rather then currently active)
--- nb_ask_py/src/cellm/core.py
+++ nb_ask_py/src/cellm/core.py
@@ -17,7 +17,10 @@ def get_cells():
 
 
 def insert_cell(content):
-    httpx.post(f"{EXT_URL}:{EXT_PORT}", json={"content": content})
+    return httpx.post(
+        f"{EXT_URL}:{EXT_PORT}/insert_response",
+        json={"content": content},
+    )
 
 
 load_dotenv()
@@ -27,8 +30,13 @@ endpoint = os.environ["OPENROUTER_ENDPOINT"]
 key = os.environ["OPENROUTER_API_KEY"]
 model = os.environ["MODEL"]
 
-sp = """Allways be helpful and suggestive. Make an efford to always give the shortest answer possible without losing correctness. In addition to the actual promt you are given the content of the jupyter notebook you are currently in - it's marked by `active notebook content:`. Each item of the json serialized list represents one cell consisting of `index` which gives you the execution order; `kind` so if its a code or a markdown cell; `source` is the user input to the cell; `output`
- is the result of the cell execution."""
+sp = """
+Allways be helpful and suggestive. Make an efford to always give the shortest answer possible without losing correctness.
+In addition to the actual promt you are given the content of the jupyter notebook you are currently in - it's marked by `active notebook content:`.
+Your output is automatically marked by this header: "### 🤖 LLM Response". Do not mimic that formatting.
+Each item of the json serialized list represents one cell consisting of `index` which gives you the execution order; `kind` so if its a code or a markdown cell;
+`source` is the user input to the cell; `output` is the result of the cell execution.
+"""
 
 chat = Chat(model=model, api_base=endpoint, api_key=key, sp=sp)
 
@@ -39,5 +47,7 @@ def ask(pr: str):
     resp = chat(pr)
     chat.last_response = resp
 
-    display(Markdown(resp.choices[0].message.content))
-    insert_cell(resp.choices[0].message.content)
+    model_name = chat.model.split("/")[-1]
+    header = f"##### 🤖 LLM Response ({model_name} | in: {resp.usage.prompt_tokens} | out: {resp.usage.completion_tokens})\n\n> "
+    quoted = resp.choices[0].message.content.replace("\n", "\n> ")
+    insert_cell(header + quoted)
